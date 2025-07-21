89b62a7c089b069de8752893a9eab8358c80ff8d
Zack Cornelius <zcorneli.iit@gmail.com>
2025-07-20 23:28:26 -0500

    Fixup completed task recognition

--- apps/api/src/task/task.service.ts
+++ apps/api/src/task/task.service.ts
@@ -4,12 +4,23 @@ import { TodoistService } from '../todoist/todoist.service';
 import fs, { createReadStream } from 'fs';
 import path from 'path';
 
-const afterToday = (date: Date): boolean => {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-  today.setUTCHours(0, 0, 0, 0);
-  return date > today;
+
+const afterDate = (date: Date, after?: Date, utc = false): boolean => {
+  if (after == null) {
+    after = new Date()
+  }
+  after = new Date(after);
+  if (utc) {
+    after.setHours(0, 0, 0, 0);
+    after.setUTCHours(23,59,59,999);
+  } else {
+    after.setHours(23,59,59,999)
+  }
+  console.log(`Comparing ${date.toISOString()} > ${after.toISOString()}`);
+  return date > after;
 }
+
+
 
 @Injectable()
 export class TaskService {
@@ -18,6 +29,7 @@ export class TaskService {
   async getTasks(options: {userId?: string, date?: Date, filter?: string} = {}): Promise<Array<Task>> {
     const client = this.todoistService.client;
     const taskQuery = { projectId: process.env.TODOIST_PROJECT_KEY };
+    let labels = []
 
     if (options.userId != null) {
       taskQuery['sectionId'] = options.userId;
@@ -30,7 +42,9 @@ export class TaskService {
     } else {
       const date = new Date();
       let timeFilter = ' & (@morning | !@afternoon)'
+      labels = ['morning']
       if (date.getHours() >= 13) {
+        labels = ['afternoon']
         timeFilter = ' & (@afternoon | !@morning)'
       }
       taskQuery['filter'] = 'due today' + timeFilter;
@@ -38,18 +52,19 @@ export class TaskService {
 
     const tasks = await client.getTasks({ ...taskQuery, projectId: process.env.TODOIST_PROJECT_KEY });
 
-    console.log(tasks);
+    //console.log(tasks);
 
     const headers = new Headers();
     headers.append('Authorization', `Bearer ${process.env.TODOIST_API_KEY}`);
     headers.append('Content-Type', 'application/json');
 
     const data = new URLSearchParams();
-    data.append('resource_types', '["items"]');
+    data.append('resource_types', '["user_plan_limits"]');
     data.append('sync_token', '*');
 
+    
     /*
-    const result = await fetch('https://api.todoist.com/sync/v9/sync', {
+    const results = await fetch('https://api.todoist.com/sync/v9/sync', {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${process.env.TODOIST_API_KEY}`,
@@ -60,13 +75,75 @@ export class TaskService {
 
 
     //console.log(result);
-    const json = await result.json();
-    //console.log(json.items);
+    const json = await results.json();
+    console.log(json);
     */
+    const object_ids: Record<string, boolean> = {};
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    let cursor = undefined;
+    let foundOld = false;
+    while (!foundOld) {
+      const params = new URLSearchParams({
+        object_type: 'item',
+        page: '0',
+        event_type: 'completed',
+        annotate_notes: 'true',
+        annotate_parents: 'true',
+        limit: '30',
+        parent_project_id: process.env.TODOIST_PROJECT_KEY,
+      });
+
+      if (cursor) {
+        params.append('cursor', cursor);
+      }
+
+      const activitiesurl =
+        'https://app.todoist.com/api/v1/activities?' + params;
+
+      console.log('Fetching activities from ', activitiesurl);
+
+      const result = await fetch(activitiesurl, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${process.env.TODOIST_API_KEY}`,
+        },
+      });
+
+      console.log(JSON.stringify(tasks, null, 4));
+
+      const activityresult = await result.json();
+      cursor = activityresult.next_cursor;
+
+      console.log(JSON.stringify(activityresult, null, 4))
+
+      for (const completed of activityresult.results) {
+        if (!afterDate(new Date(completed.event_date), yesterday)) {
+          // Results are reverse timestamp ordered
+          foundOld = true
+          break;
+        }
+        if (object_ids[completed.object_id]) {
+          continue;
+        }
+        object_ids[completed.object_id] = true;
+        const task = await client.getTask(completed.object_id);
+        if (task && task.labels.some((label) => labels.includes(label))) {
+          const existingTask = tasks.find((t) => t.id === task.id);
+          if (!existingTask) {
+            task.isCompleted =
+              task.isCompleted || afterDate(new Date(task.due?.date), null, true);
+            tasks.push(task);
+          }
+        }
+      }
+    }
    
     const date = options.date ?? new Date();
     date.setHours(0, 0, 0, 0);
 
+    /*
     const completedResult = await fetch('https://api.todoist.com/sync/v9/completed/get_all?' + new URLSearchParams({ since: date.toISOString() }), {
       method: 'GET',
       headers: {
@@ -75,7 +152,7 @@ export class TaskService {
     });
 
     const completedTasks = await completedResult.json();
-    console.log('Completed tasks:', completedTasks);
+    //console.log('Completed tasks:', completedTasks);
 
 
     for (const task of completedTasks.items) {
@@ -89,6 +166,7 @@ export class TaskService {
         tasks.push(new_task);
       }
     }
+    */
 
     return tasks;
   }
@@ -102,7 +180,7 @@ export class TaskService {
 
     const task = await client.getTask(id);
     if (task.due?.isRecurring) {
-      task.isCompleted = afterToday(new Date(task.due?.date))
+      task.isCompleted = afterDate(new Date(task.due?.date), null, true)
     }
 
     return task
